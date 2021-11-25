24ad26b7a0e992b9a997b50c99ef4ca0f90424e7
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-11-26 04:47:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -567,8 +567,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 
             await query.message.edit(text="<b>Developer : <a href='https://t.me/dent_tech_for_books'>📚🅳🆃 📖 🆁🅾🅾🅼📚</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://t.me/dent_tech_library'>🔬𝔻𝕖𝕟𝕥 𝕋𝕖𝕔𝕙 𝕃𝕚𝕓𝕣𝕒𝕣𝕪📚</a>\nDiscussion Group: <a href='https://t.me/dent_tech_for_u'>Click Here</a>\n𝗝𝗼𝗶𝗻 🦷𝔻𝕖𝕟𝕥𝕒𝕝 ℂ𝕒𝕤𝕖 𝕊𝕥𝕦𝕕𝕪🔎: <a href='https://t.me/dental_case_study'>Click Here</a></b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
             
-            )
-            return
+                )
+                return
         elif query.data == "stats":
             buttons = [[
                 InlineKeyboardButton('👩‍🦯 Back', callback_data='start'),
