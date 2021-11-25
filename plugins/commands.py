eee79db121e506e85b7cf1a26c4e9f265ad38603
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-11-25 09:24:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -102,7 +102,7 @@ async def start(bot, cmd):
             photo=random.choice(PICS),
             
             caption=script.START_MSG
-            parse_mode="Markdown"
+            parse_mode="html",
             
             reply_markup=InlineKeyboardMarkup(
                 [
