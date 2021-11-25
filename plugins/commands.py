5361bc97087c047df5f4e16b4952ba4ac1c3a922
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-11-25 08:15:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import random
+from Script import script
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from info import START_MSG, CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, PICS
