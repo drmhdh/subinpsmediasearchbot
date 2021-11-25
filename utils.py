cffa00e5398d6a9e53d25e47053f20fa92f7858a
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-11-25 14:54:36 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -89,7 +89,18 @@ async def search_gagala(text):
     titles = soup.find_all( 'h3' )
     return [title.getText() for title in titles]  
 
+def get_size(size):
+    """Get size in readable format"""
 
+units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
+size = float(size)
+i = 0
+while size >= 1024.0 and i < len(units):
+    i += 1
+    size /= 1024.0
+return "%.2f %s" % (size, units[i])
+    
+    
         
 @imdb.register
 class Poster(Document):
