3b3cac16b7f995822fb977241986bfd80e7a4f8e
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-11-25 08:18:08 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,4 @@
+class script(object):
+    START_TXT = """𝙷𝙴𝙻𝙾 {},
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/search_dentalbooks_bot> 📚𝙳𝚎𝚗𝚝 𝚃𝚎𝚌𝚑 🔎 𝙻𝚒𝚋𝚛𝚊𝚛𝚒𝚊𝚗🧚‍♀</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝚈𝙾𝚄 𝙰𝚅𝙰𝙸𝙻𝙰𝙱𝙻𝙴 𝙳𝙴𝙽𝚃𝙰𝙻 𝚁𝙴𝙵𝙴𝚁𝙴𝙽𝙲𝙴𝚂 𝙵𝚁𝙴𝙴, 𝙹𝙾𝙸𝙽 𝙾𝚄𝚁 <a href='https://t.me/dent_tech_for_books'> 📚 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 🔎 𝙶𝚁𝙾𝚄𝙿 🧚‍♀ </a> 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
+    
