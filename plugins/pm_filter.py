5b3d02bd1daca1e7b56593bd815b149218a7f11c
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-11-26 02:17:01 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -561,7 +561,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ],[
                 InlineKeyboardButton('🔮 Status', callback_data='stats')    
                 ],[                
-                InlineKeyboardButton('🏠 Home', callback_data='subinps'),
+                InlineKeyboardButton('🏠 Home', callback_data='start'),
                 InlineKeyboardButton('🔐 Close', callback_data='close_data')
             ]]
                 
@@ -569,7 +569,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
          
         elif query.data == "stats":
             buttons = [[
-                InlineKeyboardButton('👩‍🦯 Back', callback_data='help'),
+                InlineKeyboardButton('👩‍🦯 Back', callback_data='start'),
                 InlineKeyboardButton('♻️', callback_data='rfrsh')
             ]]
             reply_markup = InlineKeyboardMarkup(buttons)
@@ -588,7 +588,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "rfrsh":
             await query.answer("Fetching MongoDb DataBase")
             buttons = [[
-                InlineKeyboardButton('👩‍🦯 Back', callback_data='subinps'),
+                InlineKeyboardButton('👩‍🦯 Back', callback_data='start'),
                 InlineKeyboardButton('♻️', callback_data='rfrsh')
             ]]
             reply_markup = InlineKeyboardMarkup(buttons)
