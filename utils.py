26dab73148c3bbecf9d2b7082cda5d6742e62776
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-11-25 11:10:54 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ import os
 from datetime import datetime
 from typing import List
 from pyrogram.types import InlineKeyboardButton
-from database.users_chats_db import db
+
 from bs4 import BeautifulSoup
 import requests
 
