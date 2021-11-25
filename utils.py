84697c9fc04be68d00195429ff5763adb75e6b15
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-11-25 15:08:19 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -89,16 +89,7 @@ async def search_gagala(text):
     titles = soup.find_all( 'h3' )
     return [title.getText() for title in titles]  
 
-def get_size(size):
-    """Get size in readable format"""
 
-units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
-size = float(size)
-i = 0
-while size >= 1024.0 and i < len(units):
-    i += 1
-    size /= 1024.0
-return "%.2f %s" % (size, units[i])
     
 def get_file_id(msg: Message):
     if msg.media:
