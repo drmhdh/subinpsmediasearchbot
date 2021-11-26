faa2954708c6d75805c0d821d8041c6f440ced49
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-11-26 14:05:03 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,7 @@
 class script(object):
     START_MSG = """𝙷𝙴𝙻𝙾 {},
 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/search_dentalbooks_bot> 📚𝙳𝚎𝚗𝚝 𝚃𝚎𝚌𝚑 🔎 𝙻𝚒𝚋𝚛𝚊𝚛𝚒𝚊𝚗🧚‍♀</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝚈𝙾𝚄 𝙰𝚅𝙰𝙸𝙻𝙰𝙱𝙻𝙴 𝙳𝙴𝙽𝚃𝙰𝙻 𝚁𝙴𝙵𝙴𝚁𝙴𝙽𝙲𝙴𝚂 𝙵𝚁𝙴𝙴, 𝙹𝙾𝙸𝙽 𝙾𝚄𝚁 <a href='https://t.me/dent_tech_for_books'> 📚 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 🔎 𝙶𝚁𝙾𝚄𝙿 🧚‍♀ </a> 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍
+
 😉 ʜᴇʀᴇ ʏᴏᴜ ᴄᴀɴ sᴇᴀʀᴄʜ ғɪʟᴇs ɪɴ ɪɴʟɪɴᴇ ᴍᴏᴅᴇ ᴀs ᴡᴇʟʟ ᴀs ᴘᴍ, ᴜsᴇ ᴛʜᴇ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴs ᴛᴏ sᴇᴀʀᴄʜ ғɪʟᴇs ᴏʀ sᴇɴᴅ ᴍᴇ ᴛʜᴇ ɴᴀᴍᴇ ᴏғ ғɪʟᴇ ᴛᴏ sᴇᴀʀᴄʜ. 🌹"""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
 ✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href='https://t.me/dent_tech_for_books'> 📚🅳🆃 📖 🆁🅾🅾🅼📚♀</a>
