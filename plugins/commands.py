7d16ad455d2ed981954492ce40d064b1792e1339
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-11-27 20:31:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -264,7 +264,7 @@ async def bot_info(bot, message):
 async def search(bot, cmd):
     usr_cmdall1 = cmd.text
     if usr_cmdall1.startswith("/search"):
-        await client.send_message(
+        await bot.send_message(
             chat_id=cmd.from_user.id,
             text=(f"</b>Helo, {cmd.from_user.mention} \n🕹  Press Search Button and Type Your Keyword to Search Available References📖</b>"),
             reply_markup=InlineKeyboardMarkup(
