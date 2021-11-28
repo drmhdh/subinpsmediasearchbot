cc9dfa3f1937280b4d872b824b5a79f1efb81f93
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-11-28 18:25:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -148,9 +148,9 @@ async def start(bot, cmd):
     
     else:
         await asyncio.sleep(2)
-        if not await db.get_chat(cmd.chat.id):
-            total=await bot.get_chat_members_count(cmd.chat.id)
-            await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(cmd.chat.title, cmd.chat.id, total, "Unknown"))       
+        if not await db.get_chat(message.chat.id):
+            total=await bot.get_chat_members_count(message.chat.id)
+            await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
             await db.add_chat(cmd.chat.id, cmd.chat.title)
         return 
     if not await db.is_user_exist(message.from_user.id):
