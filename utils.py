a66d6716b8058317214137d56a19d0ed8995f497
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-11-28 21:39:14 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -408,5 +408,24 @@ def list_to_str(k):
         return ' '.join(f'{elem}, ' for elem in k)
     else:
         return ' '.join(f'{elem}, ' for elem in k)
+def last_online(from_user):
+    time = ""
+    if from_user.is_bot:
+        time += "🤖 Bot :("
+    elif from_user.status == 'recently':
+        time += "Recently"
+    elif from_user.status == 'within_week':
+        time += "Within the last week"
+    elif from_user.status == 'within_month':
+        time += "Within the last month"
+    elif from_user.status == 'long_time_ago':
+        time += "A long time ago :("
+    elif from_user.status == 'online':
+        time += "Currently Online"
+    elif from_user.status == 'offline':
+        time += datetime.fromtimestamp(from_user.last_online_date).strftime("%a, %d %b %Y, %H:%M:%S")
+    return time
 
+    
+    
         
