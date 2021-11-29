fca4879be8115968ad8dbeadfb168b822d955395
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-11-29 18:12:36 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -326,7 +326,7 @@ def split_list(l, n):
 
 @Client.on_callback_query()
 
-"""async def auto_filter(client, msg, spoll=False):
+async def auto_filter(client, msg, spoll=False):
     if not spoll:
         message = msg
         if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
@@ -360,7 +360,7 @@ def split_list(l, n):
                 ),
             ]
             for file in files
-        ]"""
+        ]
 
 
 
