40246c23e574d1c90167b5f65827a2d4fd52d1fc
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-11-29 09:09:35 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -3,7 +3,7 @@ import os
 import logging
 import random
 from Script import script
-from pyrogram import Client, filters
+from pyrogram import filters, Client 
 from pyrogram.errors.exceptions.bad_request_400 import ChatAdminRequired
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from info import START_MSG, CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, PICS, LOG_CHANNEL
