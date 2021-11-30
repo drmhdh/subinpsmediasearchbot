9cef4f38ad2ac1e9ec2b5a241521f1ccbe12781e
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-11-30 17:51:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -748,93 +748,93 @@ async def cb_handler(client: Client, query: CallbackQuery):
    
    
    
-        elif query.data.startswith("subinps"):
-            ident, file_id = query.data.split("#")
-            filedetails = await get_file_details(file_id)
-            for files in filedetails:
-                title = files.file_name
-                size=get_size(files.file_size)
-                f_caption=files.caption
-                if CUSTOM_FILE_CAPTION:
-                    try:
-                        f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
-                    except Exception as e:
-                        print(e)
-                        f_caption=f_caption
-                if f_caption is None:
-                    f_caption = f"{files.file_name}"
-                buttons = [
-                    [
-                    InlineKeyboardButton('🔎 Search', switch_inline_query_current_chat='')   
-                    ]
-                    ,
-                    [
-                    InlineKeyboardButton('📚🅳🆃 📖 🆁🅾🅾🅼📚', url='https://t.me/dent_tech_for_books')
-                    ]
-                    ,
-                    [
-                    InlineKeyboardButton('𝗝𝗼𝗶𝗻 🦷𝔻𝕖𝕟𝕥𝕒𝕝 ℂ𝕒𝕤𝕖 𝕊𝕥𝕦𝕕𝕪🔎', url='https://t.me/dental_case_study')
-                    ]
-                    ]
+    elif query.data.startswith("subinps"):
+        ident, file_id = query.data.split("#")
+        filedetails = await get_file_details(file_id)
+        for files in filedetails:
+            title = files.file_name
+            size=get_size(files.file_size)
+            f_caption=files.caption
+            if CUSTOM_FILE_CAPTION:
+                try:
+                    f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+                except Exception as e:
+                    print(e)
+                    f_caption=f_caption
+            if f_caption is None:
+                f_caption = f"{files.file_name}"
+            buttons = [
+                [
+                InlineKeyboardButton('🔎 Search', switch_inline_query_current_chat='')   
+                ]
+                ,
+                [
+                InlineKeyboardButton('📚🅳🆃 📖 🆁🅾🅾🅼📚', url='https://t.me/dent_tech_for_books')
+                ]
+                ,
+                [
+                InlineKeyboardButton('𝗝𝗼𝗶𝗻 🦷𝔻𝕖𝕟𝕥𝕒𝕝 ℂ𝕒𝕤𝕖 𝕊𝕥𝕦𝕕𝕪🔎', url='https://t.me/dental_case_study')
+                ]
+                ]
                 
-                await query.answer()
-                await client.send_cached_media(
-                    chat_id=query.from_user.id,
-                    file_id=file_id,
-                    caption=f_caption,
-                    reply_markup=InlineKeyboardMarkup(buttons)
-                    )
-        elif query.data.startswith("checksub"):
-            if AUTH_CHANNEL and not await is_subscribed(client, query):
-                await query.answer("I Like Your Smartness, But Don't Be Oversmart 😒",show_alert=True)
-                return
-            ident, file_id = query.data.split("#")
-            filedetails = await get_file_details(file_id)
-            for files in filedetails:
-                title = files.file_name
-                size=get_size(files.file_size)
-                f_caption=files.caption
-                if CUSTOM_FILE_CAPTION:
-                    try:
-                        f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
-                    except Exception as e:
-                        print(e)
-                        f_caption=f_caption
-                if f_caption is None:
-                    f_caption = f"{title}"
-                buttons = [
-                    [
-                    InlineKeyboardButton('🔎 Search', switch_inline_query_current_chat='')
-                    ]
-                    ,
-                    [
-                    InlineKeyboardButton('📚🅳🆃 📖 🆁🅾🅾🅼📚', url='https://t.me/dent_tech_for_books')
-                    ]
-                    ,
-                    [
-                    InlineKeyboardButton('𝗝𝗼𝗶𝗻 🦷𝔻𝕖𝕟𝕥𝕒𝕝 ℂ𝕒𝕤𝕖 𝕊𝕥𝕦𝕕𝕪🔎', url='https://t.me/dental_case_study')
-                    ]
-                    ]
-                
-                await query.answer()
-                await client.send_cached_media(
-                    chat_id=query.from_user.id,
-                    file_id=file_id,
-                    caption=f_caption,
-                    reply_markup=InlineKeyboardMarkup(buttons)
-                    )
-
-
-        elif query.data == "pages":
             await query.answer()
-        elif query.data == "close":
-            try:
-                await query.message.reply_to_message.delete()
-                await query.message.delete()
-            except:
-                await query.message.delete()
-    else:
-        await query.answer("It Will Not Work for You, as It was Not Requested by You 😒",show_alert=True) 
+            await client.send_cached_media(
+                chat_id=query.from_user.id,
+                file_id=file_id,
+                caption=f_caption,
+                reply_markup=InlineKeyboardMarkup(buttons)
+                )
+    elif query.data.startswith("checksub"):
+        if AUTH_CHANNEL and not await is_subscribed(client, query):
+            await query.answer("I Like Your Smartness, But Don't Be Oversmart 😒",show_alert=True)
+            return
+        ident, file_id = query.data.split("#")
+        filedetails = await get_file_details(file_id)
+        for files in filedetails:
+            title = files.file_name
+            size=get_size(files.file_size)
+            f_caption=files.caption
+            if CUSTOM_FILE_CAPTION:
+                try:
+                    f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+                except Exception as e:
+                    print(e)
+                    f_caption=f_caption
+            if f_caption is None:
+                f_caption = f"{title}"
+            buttons = [
+                [
+                InlineKeyboardButton('🔎 Search', switch_inline_query_current_chat='')
+                ]
+                ,
+                [
+                InlineKeyboardButton('📚🅳🆃 📖 🆁🅾🅾🅼📚', url='https://t.me/dent_tech_for_books')
+                ]
+                ,
+                [
+                InlineKeyboardButton('𝗝𝗼𝗶𝗻 🦷𝔻𝕖𝕟𝕥𝕒𝕝 ℂ𝕒𝕤𝕖 𝕊𝕥𝕦𝕕𝕪🔎', url='https://t.me/dental_case_study')
+                ]
+                ]
+                
+            await query.answer()
+            await client.send_cached_media(
+                chat_id=query.from_user.id,
+                file_id=file_id,
+                caption=f_caption,
+                reply_markup=InlineKeyboardMarkup(buttons)
+                )
+
+
+    elif query.data == "pages":
+        await query.answer()
+    elif query.data == "close":
+        try:
+            await query.message.reply_to_message.delete()
+            await query.message.delete()
+        except:
+            await query.message.delete()
+else:
+    await query.answer("It Will Not Work for You, as It was Not Requested by You 😒",show_alert=True) 
 
 async def auto_filter(client, msg, spoll=False):
     if not spoll:
