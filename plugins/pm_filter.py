45182e95b5547d27ff36b333e0ffe43688b4b75b
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-11-30 17:24:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -139,7 +139,7 @@ async def next_page(bot, query):
         pass
     await query.answer()
       
- @Client.on_callback_query(filters.regex(r"^spolling"))
+@Client.on_callback_query(filters.regex(r"^spolling"))
 async def advantage_spoll_choker(bot, query):
     _, user, movie_ = query.data.split('#')
     if int(user) != 0 and query.from_user.id != int(user):
