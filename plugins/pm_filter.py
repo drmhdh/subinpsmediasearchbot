84a8ac5cf65d1a4bfa083e913db0f11004d57e08
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-11-30 17:53:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -833,8 +833,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await query.message.delete()
         except:
             await query.message.delete()
-else:
-    await query.answer("It Will Not Work for You, as It was Not Requested by You 😒",show_alert=True) 
+    else:
+        await query.answer("It Will Not Work for You, as It was Not Requested by You 😒",show_alert=True) 
 
 async def auto_filter(client, msg, spoll=False):
     if not spoll:
