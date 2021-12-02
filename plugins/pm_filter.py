e7c44abe2bb43c2c00a26a967712540d187371d5
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-02 14:11:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -69,7 +69,7 @@ async def give_filter(client, message):
     else:
         
         
-        await message.answer(url=f"https://t.me/{temp.U_NAME}?start={file_id}")(client, message)
+        await message.answer(url=f"https://t.me/{temp.U_NAME}?start={file_id}")
         
 
 @Client.on_message(filters.text & filters.private & filters.incoming & filters.user(AUTH_USERS) if AUTH_USERS else filters.text & filters.private & filters.incoming)
