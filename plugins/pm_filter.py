6553cdff004aed7e795dcd702ba01bab6554a730
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-02 09:17:20 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -65,16 +65,12 @@ async def give_filter(client,message):
                 break 
 
     else:
-        
-        for file in files:
-                file_id = file.file_id
+        file_id = file.file_id
                 filename = f"[{get_size(file.file_size)}] {file.file_name}"
                 btn.append(
                     [InlineKeyboardButton(text=f"{filename}",callback_data=f"subinps#{file_id}")]
                     )
-                
-
-                  
+        
 @Client.on_message(filters.text & filters.private & filters.incoming & filters.user(AUTH_USERS) if AUTH_USERS else filters.text & filters.private & filters.incoming)
 async def filter(client, message):
     if message.text.startswith("/"):
