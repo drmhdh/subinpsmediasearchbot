d11dcf18f0783826c6c2cdfc010546e5d065bc8a
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-02 09:53:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -731,3 +731,14 @@ async def cb_handler(client: Client, query: CallbackQuery):
             )     
 
 
+    elif "alertmessage" in query.data:
+        grp_id = query.message.chat.id
+        i = query.data.split(":")[1]
+        keyword = query.data.split(":")[2]
+        reply_text, btn, alerts, fileid = await find_filter(grp_id, keyword)
+        if alerts is not None:
+            alerts = ast.literal_eval(alerts)
+            alert = alerts[int(i)]
+            alert = alert.replace("\\n", "\n").replace("\\t", "\t")
+            await query.answer(alert,show_alert=True)
+       
