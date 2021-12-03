03f7548d9c29672c6ec94665957c8354cd1ac812
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-03 11:09:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -810,7 +810,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f"subinps#{file_id}'
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -820,11 +820,11 @@ async def auto_filter(client, msg, spoll=False):
             [
                 InlineKeyboardButton(
                     text=f"{file.file_name}",
-                    callback_data=callback_data=f"subinps#{file_id}',
+                    callback_data=f'files#{file.file_id}',
                 ),
                 InlineKeyboardButton(
                     text=f"{get_size(file.file_size)}",
-                    callback_data=callback_data=f"subinps#{file_id}',
+                    callback_data=f'files#{file.file_id}',
                 ),
             ]
             for file in files
