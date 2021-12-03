a2a4e6a5112b7ad223e6ae9db7df3ea305924c4f
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-03 09:32:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -855,11 +855,7 @@ async def auto_filter(client, msg, spoll=False):
         if 2 < len(message.text) < 100:
             search = message.text
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
-            if not files:
-                if SPELL_CHECK_REPLY:
-                    return await advantage_spell_chok(msg)
-                else:
-                    return
+            
         else:
             return
     else:
