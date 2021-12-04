42f8002d80346847302265dd6945ed6758648575
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-04 15:50:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -545,6 +545,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 await query.message.delete()
             except:
                 await query.message.delete()
+            return
+            st = await client.get_chat_member(grp_id, userid)
+            if (st.status == "creator") or (str(userid) in ADMINS):    
+                await close(query.message, grp_id, title)
+            else:
+                await query.answer("You need to be Group Owner or an Admin to do that!",show_alert=True)        
+        
     
                                                                               
     if query.data == "close_data":
