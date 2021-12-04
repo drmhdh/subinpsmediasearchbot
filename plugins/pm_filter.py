342ef43628191627021b75830f408d732ceb3e56
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-05 01:15:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -556,18 +556,18 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 grp_id = query.message.chat.id
                 title = query.message.chat.title
                
-                else:
-                    return
+            else:
+                return
                
-                st = await client.get_chat_member(grp_id, userid)
-                if (st.status == "creator") or (str(userid) in ADMINS):
-                    await query.message.delete()
-                    try:
-                        await query.message.reply_to_message.delete()
-                    except:
-                        pass
-                else:
-                    await query.answer("Thats not for you!!",show_alert=True)
+            st = await client.get_chat_member(grp_id, userid)
+            if (st.status == "creator") or (str(userid) in ADMINS):
+                await query.message.delete()
+                try:
+                    await query.message.reply_to_message.delete()
+                except:
+                    pass
+            else:
+                await query.answer("Thats not for you!!",show_alert=True)
     
    
    
