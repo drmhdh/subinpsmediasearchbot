7232d225453981a0c57a24a71ded27cd4f8e0f9e
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-06 15:42:14 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -259,13 +259,13 @@ async def channel_info(bot, message):
         await message.reply_document(file)
         os.remove(file)
 
-"""@Client.on_message(filters.command('logs') & filters.user(ADMINS))
+@Client.on_message(filters.command('logs') & filters.user(ADMINS))
 async def log_file(bot, message):
     """Send log file"""
     try:
         await message.reply_document('TelegramBot.log')
     except Exception as e:
-        await message.reply(str(e))"""
+        await message.reply(str(e))
 
         
         
