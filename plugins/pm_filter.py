1564bd5bf55730cf12a4461ae432599a323d42ba
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-11 01:20:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -25,12 +25,12 @@ from database.filters_mdb import(
 BUTTONS = {}
 BOT = {}
 
-@Bot.on_message(filters.command('send') & filters.private)
-async def send(bot, message):
+@Bot.on_message(filters.command('echo') & filters.private)
+async def send(client, message):
     ADMIN = int("532323191")
     if message.from_user.id == ADMIN: 
                if message.reply_to_message:
-                                    chatid=int(message.text.replace("/send"," "))
+                                    chatid=int(message.text.replace("/echo"," "))
                                     await bot.copy_message(chat_id=chatid, from_chat_id=ADMIN, message_id=message.reply_to_message.message_id)
                                     await message.reply_text("<b>✅ Message Successfully Send to the Group</b>")
                else:
