689d0b1119f4e17bafdf8b8397f086ebaa8cd126
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-11 05:14:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -41,7 +41,9 @@ async def echo(client, message):
             
         else:
             await message.reply_text("<b>That's not for you bruh 😅</b>")
-            #except Exception as e:
+    except Exception as e:
+        logger.exception(e)
+    
             #await message.reply(f"<b>Error!!</b>\n \n<code>{e}</code>")
 
 
