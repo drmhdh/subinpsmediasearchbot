ed759d995ddb8ea8af7befaed957fae633da2f4d
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-11 04:00:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -33,8 +33,14 @@ async def echo(client, message):
            
             if message.reply_to_message:
                 await message.reply_text(text=message.text.replace("/echo", " "))
+            
+         
             else:
-                await message.reply_text(text=message.text.replace("/echo", " "))                
+                await message.reply_text(text=message.text.replace("/echo", " "))     
+            else:
+               
+               await message.reply_text("<b>Use this command as the reply of any Message</b>")
+               
         else:
             await message.reply_text("<b>That's not for you bruh 😅</b>")
     except Exception as e:
