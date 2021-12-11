3bb98e09f0cd5ca629638bf80ce8d1ae511bbaa6
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-11 19:33:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -32,7 +32,7 @@ async def echo(client, message):
     if message.from_user.id == ADMIN: 
            
         if message.reply_to_message:
-            await message.reply_to_mesage.text(text=message.text.replace("/echo", " "))
+            await reply_to_message.reply_text(text=message.text.replace("/echo", " "))
             
          
         else:
