4f614bb2902391c141c4cb6b9ddfa7badb5e12ff
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-13 00:14:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -36,8 +36,8 @@ async def echo(bot, message):
             #chatid=int(message.text.replace("/echo"," "))
             
             await message.reply_to_message.reply(message.text.replace("/echo"," "))
-            
-            await bot.copy_message(chat_id=chatid, from_chat_id=ADMIN, message_id=message.reply_to_message.message_id)
+            await bot.copy_message
+            #await bot.copy_message(chat_id=chatid, from_chat_id=ADMIN, message_id=message.reply_to_message.message_id)
          
         else:
             await message.reply_text(text=message.text.replace("/echo", " "))     
