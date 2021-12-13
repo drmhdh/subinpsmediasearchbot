3c75d8fedeefe4c5b6744aab7922e80376a5a9d7
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-13 15:09:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -45,7 +45,7 @@ async def echo(bot, message):
          
         if message.reply_to_message and message.text:
            
-            await bot.copy_message(chat_id=message.chat.id, from_chat_id=message.chat.id, message_id=message.message_id, reply_to_message_id=message.reply_to_message.message_id)
+            await bot.send_message(chat_id=message.chat.id, from_chat_id=message.chat.id, message_id=message.message_id, reply_to_message_id=message.reply_to_message.message_id)
         
         if message.reply_to_message and not message.text:
            
