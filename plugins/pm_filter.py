beb31bde8ce9ca978170ce9a88c03e07e133f006
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-13 13:16:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -40,14 +40,14 @@ async def echo(bot, message):
            
         if message.reply_to_message and not message.text:
            
-            await bot.copy_message(chat_id=message.chat.id, from_chat_id=message.chat.id, message_id=message.reply_to_message.message_id, reply_to_message_id=message.reply_to_message.message_id, caption=CAP)
+            await bot.copy_message(chat_id=message.chat.id, from_chat_id=message.chat.id, message_id=message.reply_to_message.message_id, reply_to_message_id=message.reply_to_message.message_id)
         
             
         else:
-            CAP = message.caption.replace("/echo", "")
+            CAP = message.text.replace("/echo", "")
              
             title=message.caption or message.text
-            await bot.copy_message(chat_id=message.chat.id, from_chat_id=message.chat.id, message_id=message.message_id, caption=CAP or text) 
+            await bot.copy_message(chat_id=message.chat.id, from_chat_id=message.chat.id, message_id=message.message_id, caption=CAP) 
       
             
     else:
