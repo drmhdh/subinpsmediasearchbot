d56aff4522e596438b1071ada8808603b6f33244
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-13 10:22:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -36,7 +36,7 @@ async def echo(bot, message):
             await bot.copy_message(chat_id=message.chat.id, from_chat_id=message.chat.id, message_id=message.reply_to_message.message_id, reply_to_message_id=message.reply_to_message.message_id)
         
             
-        if not message.reply_to_message
+        if not message.reply_to_message:
             title=message.caption or message.text
             await bot.copy_message(chat_id=message.chat.id, from_chat_id=message.chat.id, message_id=message.message_id, caption=title.replace("/echo", " ")) 
       
