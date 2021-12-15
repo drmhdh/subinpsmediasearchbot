c02021b5daccb1ef2a92c71b685534d8557e1596
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-15 22:24:08 +0530

    Update pdf2img.py

--- Telethonebot/Plugins/pdf2img.py
+++ Telethonebot/Plugins/pdf2img.py
@@ -48,10 +48,11 @@ if not os.path.isdir("pdf"):
     
     
         
-    
+@BotzHub.on(events.NewMessage(incoming=True, pattern="/pdf"))
 @BotzHub_cmd(
-    pattern="pdf ?(.*)",
-)
+    #pattern="pdf ?(.*)",
+    #pattern="/pdf",
+#)
 async def pdfseimg(event):
     ok = await event.get_reply_message()
     msg = event.pattern_match.group(1)
