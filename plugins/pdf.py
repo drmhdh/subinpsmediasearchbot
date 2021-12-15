a27835a49daad7578682b3ea78ddd4be2dff0c13
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2021-12-15 15:40:58 +0530

    Update pdf.py

--- plugins/pdf.py
+++ plugins/pdf.py
@@ -32,8 +32,78 @@ from . import *
 if not os.path.isdir("pdf"):
     os.mkdir("pdf")
 
+def ultroid_cmd(allow_sudo=should_allow_sudo(), **args):
+    # With time and addition of Stuff
+    # Decorator has turned lengthy and non attractive.
+    # Todo : Make it better..
+    args["func"] = lambda e: not e.fwd_from and not e.via_bot_id
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+    pattern = args["pattern"]
+    black_chats = args.get("chats", None)
+    groups_only = args.get("groups_only", False)
+    admins_only = args.get("admins_only", False)
+    fullsudo = args.get("fullsudo", False)
+    allow_all = args.get("allow_all", False)
+    type = args.get("type", ["official"])
+    only_devs = args.get("only_devs", False)
+    allow_pm = args.get("allow_pm", False)
+    if isinstance(type, str):
+        type = [type]
+    if "official" in type and DUAL_MODE:
+        type.append("dualmode")
+        
+    args["forwards"] = False
+    if pattern:
+        args["pattern"] = compile_pattern(pattern, hndlr)
+        reg = re.compile("(.*)")
+        try:
+            cmd = re.search(reg, pattern)
+            try:
+                cmd = (
+                    cmd.group(1)
+                    .replace("$", "")
+                    .replace("?(.*)", "")
+                    .replace("(.*)", "")
+                    .replace("(?: |)", "")
+                    .replace("| ", "")
+                    .replace("( |)", "")
+                    .replace("?((.|//)*)", "")
+                    .replace("?P<shortname>\\w+", "")
+                )
+            except BaseException:
+                pass
+            try:
+                LIST[file_test].append(cmd)
+            except BaseException:
+                LIST.update({file_test: [cmd]})
+        except BaseException:
+            pass
+    args["blacklist_chats"] = True
+    if len(black_list_chats) > 0:
+        args["chats"] = black_list_chats
+    if black_chats is not None:
+        if len(black_chats) == 0:
+            args["chats"] = []
+        else:
+            args["chats"] = black_chats
 
-@subinpsmediasearchbot_cmd(
+    for i in [
+        "admins_only",
+        "groups_only",
+        "type",
+        "allow_all",
+        "fullsudo",
+        "only_devs",
+        "allow_pm",
+    ]:
+        if i in args:
+            del args[i]
+    
+    
+@Ultroid_cmd(
     pattern="pdf ?(.*)",
 )
 async def pdfseimg(event):
